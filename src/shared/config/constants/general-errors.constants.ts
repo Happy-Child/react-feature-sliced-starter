f0f67179d0a27a0c01f7e26c5d3abd113525973e
Egor Lazuka <mozerqake1999@gmail.com>
2022-01-12 16:57:35 +0300

    [PP-2]: impl server and general errors

---
+++ src/shared/config/constants/general-errors.constants.ts
@@ -0,0 +1,4 @@
+export const GENERAL_ERRORS: Record<string, string> = {
+  ID_SHOULD_BE_UNIQUES: 'ID должен быть уникальным',
+  REQUIRED_FIELD: 'Обязательное поле',
+};
---
+++ src/shared/config/constants/index.ts
@@ -0,0 +1,2 @@
+export { SERVER_ERRORS } from './server-errors.constants';
+export { GENERAL_ERRORS } from './general-errors.constants';
